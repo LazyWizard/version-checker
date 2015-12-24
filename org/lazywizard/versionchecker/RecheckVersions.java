6fad7a44b1c96948516e5eb52b525c2c28742df6
Robert Maguire <Robert Maguire@localhost>
2015-12-23 18:50:42 -0600

    Added RecheckVersions console command

---
+++ org/lazywizard/versionchecker/RecheckVersions.java
@@ -0,0 +1,42 @@
+package org.lazywizard.versionchecker;
+
+import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.ModPlugin;
+import org.lazywizard.console.BaseCommand;
+import org.lazywizard.console.Console;
+
+public class RecheckVersions implements BaseCommand
+{
+    @Override
+    public CommandResult runCommand(String args, CommandContext context)
+    {
+        for (ModPlugin tmp : Global.getSettings().getModManager().getEnabledModPlugins())
+        {
+            if (tmp instanceof VCModPlugin)
+            {
+                VCModPlugin plugin = (VCModPlugin) tmp;
+                try
+                {
+                    plugin.onApplicationLoad();
+
+                    if (context.isInCampaign())
+                    {
+                        Global.getSector().removeScriptsOfClass(UpdateNotificationScript.class);
+                        plugin.onGameLoad(false);
+                    }
+
+                    Console.showMessage("Update check started successfully.");
+                    return CommandResult.SUCCESS;
+                }
+                catch (Exception ex)
+                {
+                    Console.showException("Something went wrong!", ex);
+                    return CommandResult.ERROR;
+                }
+            }
+        }
+
+        Console.showMessage("Couldn't find Version Checker's ModPlugin!");
+        return CommandResult.ERROR;
+    }
+}
