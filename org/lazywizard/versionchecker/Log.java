4f6c61cd4756d71ca6e5958376a5d48cc9801443
Robert Maguire <Robert Maguire@localhost>
2016-04-23 17:52:37 -0500

    Fixed infinite loop if an unchecked exception was thrown during update checks

--- org/lazywizard/versionchecker/Log.java
+++ org/lazywizard/versionchecker/Log.java
@@ -6,61 +6,61 @@ import org.apache.log4j.Logger;
 // Minor utility class to ensure all logging is done under the same class
 class Log
 {
-    private static final Logger Log = Logger.getLogger(VersionChecker.class);
+    private static final Logger _Log = Logger.getLogger(VersionChecker.class);
 
     static void setLevel(Level level)
     {
-        Log.setLevel(level);
+        _Log.setLevel(level);
     }
 
     static void info(Object message)
     {
-        Log.info(message);
+        _Log.info(message);
     }
 
     static void info(Object message, Throwable ex)
     {
-        Log.info(message, ex);
+        _Log.info(message, ex);
     }
 
     static void debug(Object message)
     {
-        Log.debug(message);
+        _Log.debug(message);
     }
 
     static void debug(Object message, Throwable ex)
     {
-        Log.debug(message, ex);
+        _Log.debug(message, ex);
     }
 
     static void warn(Object message)
     {
-        Log.warn(message);
+        _Log.warn(message);
     }
 
     static void warn(Object message, Throwable ex)
     {
-        Log.warn(message, ex);
+        _Log.warn(message, ex);
     }
 
     static void error(Object message)
     {
-        Log.error(message);
+        _Log.error(message);
     }
 
     static void error(Object message, Throwable ex)
     {
-        Log.error(message, ex);
+        _Log.error(message, ex);
     }
 
     static void fatal(Object message)
     {
-        Log.fatal(message);
+        _Log.fatal(message);
     }
 
     static void fatal(Object message, Throwable ex)
     {
-        Log.fatal(message, ex);
+        _Log.fatal(message, ex);
     }
 
     private Log()
